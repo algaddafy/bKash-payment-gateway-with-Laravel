64764339a28b5b780350d2aea4cc789499350ffa
nirobhasan-git <papon.nirob@gmail.com>
2023-11-21 14:44:25 +0600

    Fix schema error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLenght(191);
+        Schema::defaultStringLength(191);
     }
 }
