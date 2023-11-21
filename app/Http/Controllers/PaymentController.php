5edbc26fabb09dfd508eda5f29f1ec890508034d
nirobhasan-git <papon.nirob@gmail.com>
2023-11-21 20:47:26 +0600

    Complete createpayment method with config.json

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -63,4 +63,39 @@ class PaymentController extends Controller
         $path = storage_path()."/app/public/config.json";
         return json_decode(file_get_contents($path),true);
     }
+
+    public function createpayment(){
+        session_start();
+
+        // $strJsonFileContents = file_get_contents("config.json");
+        // $array = json_decode($strJsonFileContents, true);
+
+        $array = $this->_get_config_file();
+
+        $amount = $_GET['amount'];
+        $invoice = $_GET['invoice']; // must be unique
+        $intent = "sale";
+        $proxy = $array["proxy"];
+        $createpaybody=array('amount'=>$amount, 'currency'=>'BDT', 'merchantInvoiceNumber'=>$invoice,'intent'=>$intent);   
+        $url = curl_init($array["createURL"]);
+
+        $createpaybodyx = json_encode($createpaybody);
+
+        $header=array(
+            'Content-Type:application/json',
+            'authorization:'.$array["token"],
+            'x-app-key:'.$array["app_key"]
+        );
+
+        curl_setopt($url,CURLOPT_HTTPHEADER, $header);
+        curl_setopt($url,CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($url,CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($url,CURLOPT_POSTFIELDS, $createpaybodyx);
+        curl_setopt($url,CURLOPT_FOLLOWLOCATION, 1);
+        //curl_setopt($url, CURLOPT_PROXY, $proxy);
+        
+        $resultdata = curl_exec($url);
+        curl_close($url);
+        echo $resultdata;
+    }
 }
